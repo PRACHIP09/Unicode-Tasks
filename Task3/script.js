880776c31e0bc139bc7b910b87304a324a6982cf
PRACHIP09 <85801018+PRACHIP09@users.noreply.github.com>
2021-08-07 18:56:41 +0530

    Add files via upload

--- Task3/script.js
+++ Task3/script.js
@@ -2,26 +2,20 @@ const pincode = document.getElementById("pincode");
 const datepic = document.getElementById("date");
 const search = document.getElementById("search");
 const na = document.getElementById("name");
-const lo = document.getElementById("load");
+const loading = document.getElementById("load");
 const container = document.getElementById("tb");
 const tabel = document.getElementById("mytable");
 var countCenters = document.getElementById("testdata");
 var filterButton = document.getElementById("filter");
 var viewFilters = document.getElementById("viewFilters");
 tabel.style.display = "none";
-filterButton.style.display = "none";
-// viewFilters.style.display = "none";
-countCenters.style.display = "none";
 var today = new Date();
-
-search.style.display = "none";
 
 const getVaccineData = (pincod, date) => {
   var pin = pincod;
   var date = date;
 
-  lo.innerText = "Loading";
-
+  loading.innerText = "Loading";
   return (
     fetch("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=" + pin +"&date=" + date)
       // Status Code
@@ -38,17 +32,6 @@ const getVaccineData = (pincod, date) => {
       })
 
       .then((result) => {
-        console.log("No:Of Centers" + typeof result.centers.length);
-        if (result.centers.length === 0) {
-          console.log("no dAta AvaiL");
-          na.innerText = `No Slots Available For ${pin} On ${date} Please Select Other Date's`;
-          tabel.style.display = "none";
-        }
-
-        lo.innerText = "";
-        tabel.style.display = "none";
-        countCenters.style.display = "none";
-        filterButton.style.display = "";
 
         for (let x = 0; x < result.centers.length; x++) {
           const name_up = result.centers[x].name;
@@ -70,20 +53,18 @@ const getVaccineData = (pincod, date) => {
             state_name,
             fee_ty,
           ];
-          // res.sort();
+          m = result.centers[x].sessions[0];
+          res.sort();
           for (let m = 0; m < res.length; m++) {
             var dup = res[m] + "span";
-            var dupdata = res[m] + "van";
+            console.log(dup);
 
             var dup = document.createElement("td");
             newDiv.appendChild(dup);
             var dupdata = document.createTextNode(res[m]);
             dup.appendChild(dupdata);
           }
-
-          //Sessions Start
-
-          m = result.centers[x].sessions[0];
+          console.log(dup)
 
           var sess_data = [
             result.centers[x].sessions.length,
@@ -95,12 +76,13 @@ const getVaccineData = (pincod, date) => {
           console.log(sess_data);
           for (let m = 0; m < sess_data.length; m++) {
             var dup = sess_data[m] + "span";
-            var dupdata = sess_data[m] + "van";
+            
             var dup = document.createElement("td");
             newDiv.appendChild(dup);
             var dupdata = document.createTextNode(sess_data[m]);
             dup.appendChild(dupdata);
           }
+          console.log(dup,dupdata)
           container.append(newDiv);
           tabel.style.display = "";
         }
@@ -108,6 +90,7 @@ const getVaccineData = (pincod, date) => {
   );
 };
 
+        
 var auto_date = today.toISOString();
 var mod_auto_date = auto_date.split("-");
 var auto_year = mod_auto_date[0];
@@ -116,15 +99,14 @@ var auto_day = mod_auto_date[2].split("");
 var mod_auto_date = auto_day[0] + auto_day[1];
 var final_auto_date = `${auto_year}-${auto_month}-${mod_auto_date}`;
 
+//to select pincode
 pincode.addEventListener("input", function () {
-  countCenters.style.display = "none";
   if (pincode.value.length === 6) {
     console.log("change");
     var pin = pincode.value;
     if (pin === "") {
       na.innerText = "Please Provide PinCode";
       tabel.style.display = "none";
-      countCenters.style.display = "none";
       return;
     }
 
@@ -136,7 +118,7 @@ pincode.addEventListener("input", function () {
     var modified_date = `${date_spli[2]}-${date_spli[1]}-${date_spli[0]}`;
     console.log("Modified-Date:" + modified_date);
     getVaccineData(pin, modified_date);
-    getVaccineDataByState(modified_date);
+    //getVaccineDataByState(modified_date);
   } else {
     na.innerText = "";
     var Table = document.getElementById("tb");
@@ -145,6 +127,7 @@ pincode.addEventListener("input", function () {
   }
 });
 
+//to select date
 datepic.addEventListener("input", function () {
   var Table = document.getElementById("tb");
   Table.innerHTML = "";
@@ -165,263 +148,11 @@ datepic.addEventListener("input", function () {
     var modified_date = `${date_chng[2]}-${date_chng[1]}-${date_chng[0]}`;
     console.log("Modified-Date:" + modified_date);
     getVaccineData(pin, modified_date);
-  } else {
+   } 
+  else {
     na.innerText = "";
     var Table = document.getElementById("tb");
     Table.innerHTML = "";
   }
 });
-
-//
-//
-//
-//
-//
-//
-
-var stateList = document.getElementById("stateList");
-var districtList = document.getElementById("district");
-var Table = document.getElementById("tb");
-
-districtList.disabled = true;
-
-fetch("https://cdn-api.co-vin.in/api/v2/admin/location/states")
-  .then((res) => res.json())
-  .then((data) => {
-    Table.innerHTML = "";
-    na.innerText = "";
-
-    for (let z = 0; z < data.states.length; z++) {
-      var option = document.createElement("option");
-      option.text = data.states[z].state_name;
-      option.value = data.states[z].state_id;
-      stateList.add(option);
-    }
-
-    // console.table(data.states);
-  });
-
-stateList.addEventListener("change", function () {
-  lo.innerText = "Loading District";
-  tabel.style.display = "none";
-  districtList.innerText = "";
-  Table.innerHTML = "";
-  countCenters.style.display = "none";
-  search.style.display = "none";
-
-  var stateCode = stateList.value;
-  fetch(
-    `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${stateCode}`,
-  )
-    .then((response) => response.json())
-    .then((fetchDistricts) => {
-      lo.innerText = "";
-      districtList.disabled = false;
-      var option = document.createElement("option");
-      option.text = "Select District";
-      option.value = "Null";
-      districtList.add(option);
-      console.log(`Total No Of States - ${fetchDistricts.districts.length}`);
-      for (let l = 0; l < fetchDistricts.districts.length; l++) {
-        var option = document.createElement("option");
-        option.text = fetchDistricts.districts[l].district_name;
-        option.value = fetchDistricts.districts[l].district_id;
-        districtList.add(option);
-      }
-    });
-});
-
-var dat = datepic.value || final_auto_date;
-var date_spli = dat.split("-");
-var modified_date = `${date_spli[2]}-${date_spli[1]}-${date_spli[0]}`;
-districtList.addEventListener("change", function () {
-  var districtCode = districtList.value;
-  lo.innerText = "Loading Vaccination Centers Data";
-  Table.innerHTML = "";
-  console.log(districtCode);
-  fetch(
-    `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtCode}&date=${modified_date}`,
-  )
-    .then((res) => res.json())
-    .then((vaccinationCentersData) => {
-      lo.innerText = "";
-      na.innerText = "";
-      filterButton.style.display = "";
-
-      console.log(
-        "Vaccination Centes is" + vaccinationCentersData.sessions.length,
-      );
-      var countCentersVal = vaccinationCentersData.sessions.length;
-      var districtName = districtList.innerHTML;
-      countCenters.innerText = `Total Number Of Vaccination Centers Is - ${countCentersVal}`;
-      countCenters.style.display = "";
-
-      for (let m = 0; m < vaccinationCentersData.sessions.length; m++) {
-        var short = vaccinationCentersData.sessions[m];
-        var date = short.date;
-        var centerName = short.name;
-        var centerAddress = short.address;
-        var blockName = short.block_name;
-        var districtName = short.district_name;
-        var stateName = short.state_name;
-        var feeType = short.fee_type;
-        var vaccineSessions = "-";
-        var vaccineName = short.vaccine;
-        var ageLimit = short.min_age_limit;
-        var dosesAvailable = short.available_capacity;
-
-        const newDiv = document.createElement("tr");
-        newDiv.classList.add("pannel");
-
-        // console.log(centerName);
-
-        // Make Data InTo Array
-       var fetchdata = [
-          date,
-          centerName,
-          centerAddress,
-          blockName,
-          districtName,
-          stateName,
-          feeType,
-          vaccineSessions,
-          vaccineName,
-          ageLimit,
-          dosesAvailable,
-        ];
-        for (let v = 0; v < fetchdata.length; v++) {
-          var dup = fetchdata[v] + "span";
-          var dupdata = fetchdata[v] + "van";
-
-          var dup = document.createElement("td");
-          newDiv.appendChild(dup);
-          var dupdata = document.createTextNode(fetchdata[v]);
-          dup.appendChild(dupdata);
-        }
-        container.append(newDiv);
-        tabel.style.display = "";
-      }
-      search.style.display = "";
-
-    });
-});
-
-const table = document.getElementById("mytable");
-const tr = table.getElementsByTagName("tr");
-search.addEventListener("input", function search() {
-  const search = document.getElementById("search");
-  filter = search.value.toUpperCase();
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[3];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-        td.style.backgroundColor = "#fff";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }
-  }
-});
-
-// Filter Ends
-
-// Global Filters
-filterButton.addEventListener("click", function () {
-  console.log("clicked");
-  viewFilters.classList.toggle("view");
-});
-
-function startFilter(filterValue) {
-  var filterreqVal = filterValue.value;
-  filter = filterreqVal.toUpperCase();
-  console.log(filter);
-  if (filter == 18) {
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[9];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-          td.style.backgroundColor = "#ba9286";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
-    }
-  }  else if (filter == "COVAXIN") {
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[8];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-          td.style.backgroundColor = "#ba9286";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
-    }
-  } else if (filter == "COVISHIELD") {
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[8];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-          td.style.backgroundColor = "#ba9286";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
-    }
-  } 
-
-  else if (filter == "FREE") {
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[6];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-          td.style.backgroundColor = "#ba9286";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
-    }
-  } else if (filter == "ALL") {
-    filter = modified_date;
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
-    }
-  }
-}
-function endFilter(filterValue) {
-  var filterreqVal = filterValue.value;
-  filter = filterreqVal.toUpperCase();
-  console.log(filter);
-  if (filter == 45) {
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[9];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-          td.style.backgroundColor = "#ba9286";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
-    }
-  }
-}
+      
